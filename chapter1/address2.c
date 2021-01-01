fba7e14bc87a6fc6444632e9773fc0b6c7ebeb6b
Rjkomal <rkiran8860@gmail.com>
2021-01-01 06:49:07 +0530

    Question_1.2

---
+++ chapter1/address2.c
@@ -0,0 +1,37 @@
+//<----------Question-1.2-------------------------->
+#include<stdio.h>
+int main()
+{ char name[20],city[20],Char;
+  int dno,stno,pcode;
+do {
+  printf("\n <---------Welcome--------->");
+  printf("\n It is a address printing program ");
+  printf("\n Enter your name ---->");
+  scanf("%s",&name[20]);
+  printf("\n Enter your door no.---->");
+  scanf("%d",&dno);
+  printf("\n Enter your street no.---->");
+  scanf("%d",&stno);
+  printf("\n Enter your city name---->");
+  scanf("%s",&city[20]);
+  printf("\n Enter your pin code---->");
+  scanf("%d",&pcode);
+  
+  printf("\n\t <--------Your address is --------->");
+  printf("\n\t|| %s",&name[20]);
+ 
+  printf("\t\t\t||");
+  printf("\n\t|| %d,street no.%d",dno,stno);
+  
+  printf("\t||");
+  printf("\n\t|| %s - %d",&city[20],pcode);
+ 
+  printf("\t||");
+  printf("\n\t <-----------END----------------->");
+ 
+  printf("\n You want to enter a new address?(y or n)--------->");
+  scanf("%s",&Char);
+}
+while(Char=='y'||Char=='Y');
+return 0;
+} 
