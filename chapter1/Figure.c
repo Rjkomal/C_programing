7f48c17950d7c788903f7e3fc7f2eab81087496f
Rjkomal <rkiran8860@gmail.com>
2021-01-01 09:23:18 +0530

    Question_1.4

---
+++ chapter1/Figure.c
@@ -0,0 +1,15 @@
+//<---------------------------Question_1.4------------>
+#include<stdio.h>
+int main()
+{
+printf("\n <----------------The figure is ---------------->");
+printf("\n ---------");printf("\t\t\t ---------");
+printf ("\n |\t |");printf("\t\t\t |\t |");
+printf ("\n |\t |");;printf("  >>-----------> ");printf("\t |\t |");
+printf ("\n |\t |");printf ("\t\t\t |\t |");
+printf("\n ---------");printf("\t\t\t ---------");
+
+printf("\n <------------------END---------------------------->\n");
+return 0;
+
+}
