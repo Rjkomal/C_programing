28a06a90f3ec8a69ec2737748bc7a65322f391a4
Rjkomal <rkiran8860@gmail.com>
2021-01-01 08:05:42 +0530

    Question_1.3

---
+++ chapter1/Pattern.c
@@ -0,0 +1,14 @@
+/*<---------------Question_1.3---------------->*/
+#include<stdio.h>
+int main()
+{
+  printf("\n<---------------Pattern------------>");
+  printf(" \n\t *");
+  printf(" \n\t * \t *");
+  printf(" \n\t * \t * \t *");
+  printf(" \n\t * \t * \t * \t *");
+  printf("\n<------------------------END------------>");
+  
+return 0;
+}
+
